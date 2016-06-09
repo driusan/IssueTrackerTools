069e4b909bf49ab9b6e438d08313c269014c9cdb
Mike Capone <mike.capone@gmail.com>
2016-06-09 00:34:16 -0400

    Added support for multiple search values. Added temporary files to .gitignore

--- bugapp/Find.go
+++ bugapp/Find.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-func find(findType, findValue string) {
+func find(findType string, findValues []string) {
 	issues, _ := ioutil.ReadDir(string(bugs.GetIssuesDir()))
 	for idx, issue := range issues {
 		if issue.IsDir() != true {
@@ -16,26 +16,30 @@ func find(findType, findValue string) {
 		var dir bugs.Directory = bugs.GetIssuesDir() + bugs.Directory(issue.Name())
 		b := bugs.Bug{Dir: dir}
 		name := getBugName(b, idx)
-		var value string
+		var values []string
 		switch findType {
 		case "tags":
+			values = b.StringTags()
 		case "status":
-			value = b.Status()
+			values = []string{b.Status()}
 		case "priority":
-			value = b.Priority()
+			values = []string{b.Priority()}
 		case "milestone":
-			value = b.Milestone()
+			values = []string{b.Milestone()}
 		default:
 			fmt.Printf("Unknown find type: %s\n", findType)
 			return
 		}
-		if findType == "tags" {
-			if b.HasTag(bugs.Tag(findValue)) {
-				fmt.Printf("%s: %s\n", name, b.Title(findType))
+		printed := false
+		for _, findValue := range findValues {
+			for _, value := range values {
+				if value == findValue {
+					fmt.Printf("%s: %s\n", name, b.Title(findType))
+					printed = true
+				}
 			}
-		} else {
-			if value == findValue {
-				fmt.Printf("%s: %s\n", name, b.Title(findType))
+			if printed {
+				break
 			}
 		}
 	}
@@ -43,7 +47,7 @@ func find(findType, findValue string) {
 
 func Find(args ArgumentList) {
 	if len(args) < 2 {
-		fmt.Printf("Usage: %s find {tag, status, priority, milestone} findvalue\n", os.Args[0])
+		fmt.Printf("Usage: %s find {tag, status, priority, milestone} value1 [value2 ...]\n", os.Args[0])
 		return
 	}
 	switch args[0] {
@@ -54,7 +58,7 @@ func Find(args ArgumentList) {
 	case "priority":
 		fallthrough
 	case "milestone":
-		find(args[0], args[1])
+		find(args[0], args[1:])
 	default:
 		fmt.Printf("Unknown command: %s %s %s\n", os.Args[0], os.Args[1], os.Args[2])
 		return
