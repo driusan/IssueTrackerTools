11db91bd83995e5f0ecc52a609276bc520be315e
Mike Capone <mike.capone@gmail.com>
2016-06-08 23:59:03 -0400

    'find priority' and 'find milestone' implimented. 'find tag' updated to 'find tags'. Currently all 'find' operations only take one search value.

--- bugapp/Find.go
+++ bugapp/Find.go
@@ -3,51 +3,58 @@ package bugapp
 import (
 	"fmt"
 	"github.com/driusan/bug/bugs"
+	"io/ioutil"
 	"os"
-	//	"io/ioutil"
 )
 
-func findTags(tags []string) {
-	bugs := bugs.FindBugsByTag(tags)
-	for index, element := range bugs {
-		fmt.Printf("%s: %s\n", getBugName(element, index), element.Title(""))
-	}
-}
-
-func findStatus(status string) {
-	bugs := bugs.GetAllBugs()
-	count := 0
-	for _, b := range bugs {
-		if b.Status() == status {
-			count++
-			fmt.Printf("%s: %s\n", getBugName(b, count), b.Title(""))
+func find(findType, findValue string) {
+	issues, _ := ioutil.ReadDir(string(bugs.GetIssuesDir()))
+	for idx, issue := range issues {
+		if issue.IsDir() != true {
+			continue
+		}
+		var dir bugs.Directory = bugs.GetIssuesDir() + bugs.Directory(issue.Name())
+		b := bugs.Bug{Dir: dir}
+		name := getBugName(b, idx)
+		var value string
+		switch findType {
+		case "tags":
+		case "status":
+			value = b.Status()
+		case "priority":
+			value = b.Priority()
+		case "milestone":
+			value = b.Milestone()
+		default:
+			fmt.Printf("Unknown find type: %s\n", findType)
+			return
+		}
+		if findType == "tags" {
+			if b.HasTag(bugs.Tag(findValue)) {
+				fmt.Printf("%s: %s\n", name, b.Title(findType))
+			}
+		} else {
+			if value == findValue {
+				fmt.Printf("%s: %s\n", name, b.Title(findType))
+			}
 		}
 	}
 }
+
 func Find(args ArgumentList) {
-	fmt.Printf("%d\n", len(args))
-
-	for i := range args {
-		fmt.Printf("%d: %s\n", i, args[i])
-	}
-
 	if len(args) < 2 {
-		fmt.Printf("Usage: %s find {(t)ag, (s)tatus, (p)riority, (m)ilestone} searchvalue\n", os.Args[0])
+		fmt.Printf("Usage: %s find {tag, status, priority, milestone} findvalue\n", os.Args[0])
 		return
 	}
-
 	switch args[0] {
-	case "tag", "t":
-		fmt.Printf("Running tag command\n")
-		tags := []string{args[1]}
-		findTags(tags)
-	case "status", "s":
-		fmt.Printf("Running status command\n")
-		findStatus(args[1])
-	case "priority", "p":
-		fmt.Printf("Running priority command\n")
-	case "milestone", "m":
-		fmt.Printf("Running milestone command\n")
+	case "tags":
+		fallthrough
+	case "status":
+		fallthrough
+	case "priority":
+		fallthrough
+	case "milestone":
+		find(args[0], args[1])
 	default:
 		fmt.Printf("Unknown command: %s %s %s\n", os.Args[0], os.Args[1], os.Args[2])
 		return
